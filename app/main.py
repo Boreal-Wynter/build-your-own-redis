e522737cf57bbc9b1d657a2c180b47bf9a8ccbdd
Boreal-Wynter <boreal.wynter@gmail.com>
2025-08-12 12:32:18 -0500

    respond to ping

--- app/main.py
+++ app/main.py
@@ -3,10 +3,26 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    # WAIT FOR A CLIENT CONNECTION
+    '''
+    socket.connect() returns (CONN, ADDRESS).
+        CONN: Socket object that sends/recieves data on the connection.
+        ADDRESS: the address bound to the socket of the client.
+    '''
+    connection, _ = server_socket.accept()
+    # SEND A 'PONG' RESPONSE
+    '''
+    * Send data to the socket
+    * Data is sent from bytes until all is sent or error occurs.
+        * Return 'None' on success
+        * Exception is raised on error
+    bytes-like object: Object that supports buffer protocol and can export C-contiguous buffer.
+        * bytes
+        * bytearray
+        * array.array
+    '''
+    connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
     main()
-
-# testing
