4574e9d04cf4e3d6c32c51e50b701dd5bf0e9dad
Boreal-Wynter <boreal.wynter@gmail.com>
2025-08-14 16:14:11 -0500

    CLI tests (cli-test-1755206051790)

--- app/main.py
+++ app/main.py
@@ -2,26 +2,17 @@ import socket  # noqa: F401
 
 
 def main():
+    # Create a server
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # WAIT FOR A CLIENT CONNECTION
-    '''
-    socket.connect() returns (CONN, ADDRESS).
-        CONN: Socket object that sends/recieves data on the connection.
-        ADDRESS: the address bound to the socket of the client.
-    '''
+    # Wait for a client connection
     connection, _ = server_socket.accept()
-    # SEND A 'PONG' RESPONSE
-    '''
-    * Send data to the socket
-    * Data is sent from bytes until all is sent or error occurs.
-        * Return 'None' on success
-        * Exception is raised on error
-    bytes-like object: Object that supports buffer protocol and can export C-contiguous buffer.
-        * bytes
-        * bytearray
-        * array.array
-    '''
-    connection.sendall(b"+PONG\r\n")
+
+    data = connection.recv(1024)
+
+    print(f"data: {data}")
+
+    # Send a 'Pong' Response
+    connection.sendall(data)  # b"+PONG\r\n"
 
 
 if __name__ == "__main__":
