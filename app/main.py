d4e2bde0c9f25f6b34b015c958e933c0a9611af0
Boreal-Wynter <boreal.wynter@gmail.com>
2025-08-14 18:27:03 -0500

    CLI tests (cli-test-1755214023657)

--- app/main.py
+++ app/main.py
@@ -1,18 +1,30 @@
 import socket  # noqa: F401
+import threading
+
+
+def test(server_socket):
+    # Wait for a client connection
+    connection, _ = server_socket.accept()
+    while connection:
+        # Wait for 'Ping'
+        connection.recv(1024)
+        # Send a 'Pong' Response
+        connection.sendall(b"+PONG\r\n")
 
 
 def main():
     # Create a server
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # Wait for a client connection
-    connection, _ = server_socket.accept()
+    threads = []
+    for i in range(2):
+        thread = threading.Thread(target=test, args=(server_socket))
+        threads.append(thread)
 
-    while connection:
-        # Wait for 'Ping'
-        connection.recv(1024)
+    for thread in threads:
+        thread.start()
 
-        # Send a 'Pong' Response
-        connection.sendall(b"+PONG\r\n")
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
