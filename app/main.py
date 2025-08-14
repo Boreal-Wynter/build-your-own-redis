44893b6ef4be7fdb7424210aaccca871d5c05595
Boreal-Wynter <boreal.wynter@gmail.com>
2025-08-14 18:41:29 -0500

    CLI tests (cli-test-1755214889679)

--- app/main.py
+++ app/main.py
@@ -4,13 +4,14 @@ import socket  # noqa: F401
 def main():
     # Create a server
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    connection, _ = server_socket.accept()
+    connection.listen(5)
+
     # Wait for a client connection
     connection, _ = server_socket.accept()
-
     while connection:
         # Wait for 'Ping'
         connection.recv(1024)
-
         # Send a 'Pong' Response
         connection.sendall(b"+PONG\r\n")
 
