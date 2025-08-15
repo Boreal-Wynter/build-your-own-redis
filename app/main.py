8bda4a98dd85693fb024e12a897789096812574a
Boreal-Wynter <boreal.wynter@gmail.com>
2025-08-14 19:46:38 -0500

    CLI tests (cli-test-1755218798172)

--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
 import socket  # noqa: F401
+import threading
+
+
+def pong_client(connection, address):
+    while connection:
+        # Wait for 'Ping'
+        connection.recv(1024)
+        # Send a 'Pong' Response
+        connection.sendall(b"+PONG\r\n")
 
 
 def main():
     # Create a server
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # Wait for a client connection
-    connection, _ = server_socket.accept()
-
-    while connection:
-        # Wait for 'Ping'
-        connection.recv(1024)
-
-        # Send a 'Pong' Response
-        connection.sendall(b"+PONG\r\n")
+    while True:
+        # Wait for a client connection
+        connection, address = server_socket.accept()
+        c_thread = threading.Thread(target=pong_client, args=(connection, address))
+        c_thread.start()
 
 
 if __name__ == "__main__":
