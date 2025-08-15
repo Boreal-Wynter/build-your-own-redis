2bd6137ea1a5483740c4b4778f26d83d504933e2
Boreal-Wynter <boreal.wynter@gmail.com>
2025-08-14 21:39:22 -0500

    CLI tests (cli-test-1755225562739)

--- app/main.py
+++ app/main.py
@@ -5,18 +5,27 @@ import threading
 def pong_client(connection):
     while connection:
         # Wait for 'Ping'
-        connection.recv(1024)
+        ping = connection.recv(1024)
+        # get ECHO command
+        ping_list = list(ping)
+        start_index = ping_list.index("$")
+        echo_ping_list = ping_list[(start_index-1):]
+        message = "" + echo_ping_list
+        bytes_object = bytes(message, 'utf-8')
         # Send a 'Pong' Response
-        connection.sendall(b"+PONG\r\n")
+        connection.sendall(bytes_object)
 
 
 def main():
     # Create a server
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    # Actively listen for clients
     while True:
-        # Wait for a client connection
+        # Wait and creates a client connection
         conn, _ = server_socket.accept()
+        # Create thread for client
         c_thread = threading.Thread(target=pong_client, args=(conn,))
+        # Start client thread
         c_thread.start()
 
 
