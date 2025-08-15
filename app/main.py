5e5b2742ff01a7283886d0bb4c65b945e16acbcb
Boreal-Wynter <boreal.wynter@gmail.com>
2025-08-14 19:48:41 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import socket  # noqa: F401
 import threading
 
 
-def pong_client(connection):
+def pong_client(connection, address):
     while connection:
         # Wait for 'Ping'
         connection.recv(1024)
@@ -15,8 +15,8 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         # Wait for a client connection
-        connection, _ = server_socket.accept()
-        c_thread = threading.Thread(target=pong_client, args=(connection))
+        conn, addr = server_socket.accept()
+        c_thread = threading.Thread(target=pong_client, args=(conn, addr))
         c_thread.start()
 
 
